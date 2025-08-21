f1f98f2d4f93eefb46c9c806ce3efad4925f1e31
samarthbhardwaj007 <aarushh00077@gmail.com>
2025-08-21 12:38:56 +0000

    done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountid = 12307
+let accountEmail = "samarth@google.com"
+var accountPassword = "123"
+accountcity = "jaipur"
+let accountstate
+
+
+// accountid = 2 // 2 defination of same variables creat chaos
+console.log(accountid);
+
+/* 
+prefer not to use var
+besause of issue in block scope anf functional scope
+*/
+
+console.table([accountid, accountEmail, accountEmail, accountcity, accountstate]);
---
+++ 01_basics/02_datatyes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version
+// alert(3+3) // we are using node.js , not browser
+console.log(3+3) // ye chalega
+
+// code redability should be high
+
+//datatypes
+
+let name  = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+//null => standalone value
+// undifined => 
+    // symbol => uniqueness 
+
+
+//object // inhke baare mein baad mein baat karenge
+
+console.log(typeof null); // this will give us output => object, which is consider s the limitation of the language
+console.log(typeof undifined);
