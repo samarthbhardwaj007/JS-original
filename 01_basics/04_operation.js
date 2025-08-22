9d3f5dda00ec6086ec6d7e376c20d012f2d12bae
samarthbhardwaj007 <aarushh00077@gmail.com>
2025-08-22 10:45:01 +0000

    done

---
+++ 01_basics/04_operation.js
@@ -0,0 +1,32 @@
+let value = 3
+let negvalue = -value
+// console.log(negvalue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2) // it is power
+// console.log(2%2)
+
+let str = "hello "
+let strr = "samarth"
+let strrr = str + strr
+// console.log(strrr)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+// console.log((3 + 4) * 5 % 3)
+
+console.log(+true)
+console.log(+"")
+
+console.log(true)
+console.log("")
+
+let gamecounter = 100
+// gameCounter++ // this will not work
+++gamecounter
+console.log(gamecounter)
