6a648043be4df565a839059bcd676e5f257276b5
samarthbhardwaj007 <aarushh00077@gmail.com>
2025-08-22 11:06:15 +0000

    done

---
+++ 01_basics/05_comparision.js
@@ -0,0 +1,27 @@
+// console.log(2 > 1)
+// console.log(2 >= 1)
+// console.log(2 < 1)
+// console.log(2 <= 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+console.log(null > 0)
+console.log(null == 0) // equality check do not convert null into the number
+console.log(null >= 0) // here comparison will convert it into the number 
+
+console.log()
+
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined < 0)
+
+console.log()
+
+// === strict check
+// it will check the datatype also
+console.log("2" === 2)
+console.log("2" == 2)
+
