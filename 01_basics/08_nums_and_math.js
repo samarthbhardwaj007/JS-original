a47e47452c55e30b2dfb4cc3923e256571414a95
samarthbhardwaj007 <aarushh00077@gmail.com>
2025-08-25 16:03:17 +0000

    done

---
+++ 01_basics/08_nums_and_math.js
@@ -0,0 +1,32 @@
+const score = 400
+// console.log(score)
+const balance = new Number(100)
+// console.log(balance)
+// console.log()
+// console.log(balance.toString())
+// console.log(balance.toFixed(2))
+
+const otherNumber =23.8966
+// console.log(otherNumber.toPrecision(3))
+// console.log(otherNumber.toPrecision(4))
+
+const hundred = 1000000
+//console.log(hundred.toLocaleString('en-IN'))
+
+// MATHS
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.6578))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.6))
+// console.log(Math.min(4,3,6,2,3,1))
+// console.log(Math.max(4,3,6,2,3,1))
+
+console.log(Math.random())
+console.log((Math.random()*10)+1)
+
+const min = 10
+const max = 20
+console.log((Math.floor(Math.random() * (max - min +1)) + min))
+
