14bcfe641a68c8e0423a61a6eb096722c3a0b619
samarthbhardwaj007 <aarushh00077@gmail.com>
2025-08-25 16:34:17 +0000

    done

---
+++ 01_basics/09_date_and_time.js
@@ -0,0 +1,13 @@
+// date
+
+let mydate = new Date()
+// console.log(mydate)
+// console.log(mydate.toString())
+// console.log(mydate.toLocaleDateString())
+// console.log(mydate.toDateString())
+// console.log(typeof mydate)
+
+let creatMyDate = new Date(2023, 0, 23) // months in javascrit start from zero
+// console.log(creatMyDate.toDateString())
+let creatMyDate2 = new Date("2003-01-14");  // ISO format
+console.log(creatMyDate2.toDateString());   // ✅
