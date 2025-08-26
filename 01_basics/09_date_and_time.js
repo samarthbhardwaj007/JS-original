13323574a3bc83ee7de6905899cdb79c24270971
samarthbhardwaj007 <aarushh00077@gmail.com>
2025-08-26 12:05:09 +0000

    done

--- 01_basics/09_date_and_time.js
+++ 01_basics/09_date_and_time.js
@@ -10,4 +10,16 @@ let mydate = new Date()
 let creatMyDate = new Date(2023, 0, 23) // months in javascrit start from zero
 // console.log(creatMyDate.toDateString())
 let creatMyDate2 = new Date("2003-01-14");  // ISO format
-console.log(creatMyDate2.toDateString());   // ✅
+// console.log(creatMyDate2.toDateString());   // ✅
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp)
+// console.log(creatMyDate.getTime())
+
+// console.log(Math.floor(Date.now()/1000))
+
+let newDate = new Date()
+console.log(newDate)
+console.log(newDate.getMonth())
+console.log(newDate.getDay())
